521068f0dc210467fd4a9a50725cb0ef559ff1f8
hankgood95 <hankgood958@gmail.com>
2021-12-12 23:25:11 +0900

    테스트 완료 하였고 이제 restAPi를 만들면 됨

---
+++ Weather/src/main/java/com/wook/controller/ReadWeatherController.java
@@ -0,0 +1,6 @@
+package com.wook.controller;
+
+public class ReadWeatherController {
+
+	//이제 여기다가 API를 만들면됨
+}
--- Weather/src/main/java/com/wook/run/Application.java
+++ Weather/src/main/java/com/wook/run/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
-@SpringBootApplication(scanBasePackages= {"com.wook.controller","com.wook.model","com.wook.weather","com.wook.service"})
+@SpringBootApplication(scanBasePackages= {"com.wook.controller","com.wook.model","com.wook.service"})
 public class Application {
 
 	public static void main(String[] args) {
