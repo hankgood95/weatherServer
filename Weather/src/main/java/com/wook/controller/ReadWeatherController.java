7189b56399e6ae1f1b65be72088535eb80b2a700
hankgood95 <hankgood958@gmail.com>
2021-12-13 23:21:08 +0900

    restapi 가닥을 약간 잡음

--- Weather/src/main/java/com/wook/controller/ReadWeatherController.java
+++ Weather/src/main/java/com/wook/controller/ReadWeatherController.java
@@ -1,6 +1,15 @@
 package com.wook.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ReadWeatherController {
 
 	//이제 여기다가 API를 만들면됨
+	@GetMapping("/todayMaxMin")
+	public String readWeather() {
+		return "this is weather info";
+	}
+	
 }
