77c83106a55129974e6cea67c45fe23e7463d5ba
hankgood95 <hankgood958@gmail.com>
2021-12-16 00:43:40 +0900

    주석달았는데 왜 안올라가지징

--- Weather/src/main/java/com/wook/model/dao/TempDao.java
+++ Weather/src/main/java/com/wook/model/dao/TempDao.java
@@ -6,7 +6,7 @@ import com.wook.model.dto.Temperature;
 
 @Mapper
 public interface TempDao {
-	int insertTemp(Temperature temp);
+	int insertTemp(Temperature temp); //온도 DB 입력
 
-	Temperature getTemp(String tempKey);
+	Temperature getTemp(String tempKey); //온도 조회
 }
