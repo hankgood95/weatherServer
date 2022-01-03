29e8d43d2f20256e9881ea6736ccdfcc6483ed53
hankgood95 <hankgood958@gmail.com>
2022-01-03 20:58:10 +0900

    매니저 키 확인하는 부분 만들었음

--- Weather/src/main/java/com/wook/controller/MemberController.java
+++ Weather/src/main/java/com/wook/controller/MemberController.java
@@ -9,15 +9,29 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.wook.model.dto.MemberResponse;
 import com.wook.model.dto.TempMember;
+import com.wook.service.MemberService;
 
 @RestController
 public class MemberController {
 
 	private Logger logger = LoggerFactory.getLogger(ShortWeatherController.class); //로그를 찍기 위해서 사용하는 Class
+	private MemberService ms;
 	
+	public MemberController(MemberService ms) {
+		super();
+		this.ms = ms;
+	}
+
 	//내가 MemberController에서 할것은
 	//1. 회원의 이메일을 받아오는 PostReqeust를 만들면 된다.
-	public MemberResponse insertMember() {
+	//2. 
+	
+	//1. 관리자가 관리자 서비스키는 Header에 담고 회원의 이메일은 body에 담아 매니저의 키가 맞는지 확인한 후에 맞다면 insert한다.
+	@PostMapping("maxmintemp/member")
+	public MemberResponse insertMember(@RequestHeader String managerKey, @RequestBody TempMember tm) {
+		
+		TempMember manager = ms.checkManager(managerKey);
+		
 		return null;
 	}
 }
---
+++ Weather/src/main/java/com/wook/service/MemberService.java
@@ -0,0 +1,23 @@
+package com.wook.service;
+
+import org.springframework.stereotype.Service;
+
+import com.wook.model.dao.MemberDao;
+import com.wook.model.dto.TempMember;
+
+@Service
+public class MemberService {
+
+	private MemberDao md;
+	
+	public MemberService(MemberDao md) {
+		super();
+		this.md = md;
+	}
+	
+	//매니저의 키를 확인하는 부분
+	public TempMember checkManager(String managerKey) {
+		return null;
+	}
+	
+}
