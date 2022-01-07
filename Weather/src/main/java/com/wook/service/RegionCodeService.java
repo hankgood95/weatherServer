1c1fce020705572e6cfc6ef24a07a88e717e08e4
hankgood95 <hankgood958@gmail.com>
2022-01-08 05:37:20 +0900

    일단 구역코드 get하는것까지 완료 했는데 DB를 수정해야함

--- Weather/src/main/java/com/wook/service/RegionCodeService.java
+++ Weather/src/main/java/com/wook/service/RegionCodeService.java
@@ -2,8 +2,21 @@ package com.wook.service;
 
 import org.springframework.stereotype.Service;
 
+import com.wook.model.dao.RegionCodeDao;
+import com.wook.model.dto.RegionCode;
+
 @Service
 public class RegionCodeService {
 	
+	private RegionCodeDao rcd;
 
+	public RegionCodeService(RegionCodeDao rcd) {
+		super();
+		this.rcd = rcd;
+	}
+	
+	public RegionCode getRegionCode(RegionCode rc) {
+		return rcd.getRegionCode(rc);
+	}
+	
 }
